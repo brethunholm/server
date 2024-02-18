4365deb4e5baced25902e43cde75c3e4505d33b9
Bre Thunholm <bthunholm@gmail.com>
2024-02-18 10:05:28 -0600

    Update res

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'see ya' });
 });
 
-// add comment
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
